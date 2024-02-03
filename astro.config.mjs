f4b078b5f4d471ce9e48134589adbf7e62df6b8f
hoach-linux <hoachprogrammer@gmail.com>
2024-02-03 21:29:37 +0700

    redesigned

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,15 @@ import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    plugins: ['@astrojs/svelte'],
-    integrations: [svelte(), react(), vue()],
-    vite: {
-        ssr: {
-            noExternal: [
-                "@smui/drawer",
-                "@smui/common",
-                "@smui/list",
-                "@smui/button",
-                "@mui/material",
-            ],
-        },
-    },
+  plugins: ['@astrojs/svelte'],
+  integrations: [svelte(), react(), vue(), tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["@smui/drawer", "@smui/common", "@smui/list", "@smui/button", "@nextui-org/button"]
+    }
+  }
 });
