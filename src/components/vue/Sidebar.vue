eed024ac9c1d7f6ed365d368246d7f4012f9d747
hoach-linux <hoachprogrammer@gmail.com>
2024-02-09 21:57:57 +0700

    fixed ug

--- src/components/vue/Sidebar.vue
+++ src/components/vue/Sidebar.vue
@@ -1,23 +1,21 @@
 <template>
   <v-navigation-drawer v-model="$isSideBarOpen">
-    <v-list>
-      <v-list density="compact" nav>
-        <v-list-item
-          prepend-icon="mdi-home-city"
-          title="Home"
-          value="home"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account"
-          title="My Account"
-          value="account"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account-group-outline"
-          title="Users"
-          value="users"
-        ></v-list-item>
-      </v-list>
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-home-city"
+        title="Home"
+        value="home"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account"
+        title="My Account"
+        value="account"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-account-group-outline"
+        title="Users"
+        value="users"
+      ></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
