5e1be26ac659ed9bd8306e2d6d50d1d5f7c861a3
hoach-linux <hoachprogrammer@gmail.com>
2024-02-10 17:46:30 +0700

    changed output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,24 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   plugins: ['@astrojs/svelte'],
-  integrations: [svelte(), react(), vue({ appEntrypoint: '/src/pages/_app' }), tailwind()],
+  integrations: [svelte(), react(), vue({
+    appEntrypoint: '/src/pages/_app'
+  }), tailwind()],
   vite: {
     ssr: {
-      noExternal: ["@smui/drawer",  "@smui/list", "@smui/button",],
-    },
-  }
+      noExternal: ["@smui/drawer", "@smui/list", "@smui/button"]
+    }
+  },
+  output: "hybrid",
+  adapter: netlify({
+    edgeMiddleware: true,
+    cacheOnDemandPages: true,
+  })
 });
