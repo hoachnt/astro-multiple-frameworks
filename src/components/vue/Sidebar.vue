a4386b3429ca4605cb17aa47e0c0c6724b661cd8
hoach-linux <hoachprogrammer@gmail.com>
2024-02-10 20:23:10 +0700

    redesgiend

--- src/components/vue/Sidebar.vue
+++ src/components/vue/Sidebar.vue
@@ -2,19 +2,28 @@
   <v-navigation-drawer v-model="$isSideBarOpen" expand-on-hover rail>
     <v-list density="compact" nav>
       <v-list-item
+        prepend-icon="mdi-chevron-left"
+        nav
+        @click.stop="isSidebarOpen.set(false)"
+      >
+      </v-list-item>
+      <v-list-item
         prepend-icon="mdi-home-city"
         title="Home"
         value="home"
+        @click.stop="isSidebarOpen.set(false)"
       ></v-list-item>
       <v-list-item
         prepend-icon="mdi-account"
         title="My Account"
         value="account"
+        @click.stop="isSidebarOpen.set(false)"
       ></v-list-item>
       <v-list-item
         prepend-icon="mdi-account-group-outline"
         title="Users"
         value="users"
+        @click.stop="isSidebarOpen.set(false)"
       ></v-list-item>
     </v-list>
   </v-navigation-drawer>
